1261e3cf5a823af204e4db6f7c61c27b8d8755ae
Aleksandr Gritsay <aleksandr.gritsay@gmail.com>
2020-12-07 18:17:11 +0600

    request weather data

--- app/src/main/java/ru/omgtu/myschoolsample/MainActivity.java
+++ app/src/main/java/ru/omgtu/myschoolsample/MainActivity.java
@@ -1,11 +1,19 @@
 package ru.omgtu.myschoolsample;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import com.androdocs.httprequest.HttpRequest;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.loader.content.AsyncTaskLoader;
 
 import android.util.Log;
 import android.view.View;
@@ -15,7 +23,19 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.channels.AsynchronousChannelGroup;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+   private final String CITY = "Omsk,ru";
+   private final String API = "8118ed6ee68db2debfaaa5a44c832918";
+   private final String LOG_TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +55,8 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(LOG_TAG, textView.getText().toString());
             }
         });
+
+       new WeatherTask().execute();
     }
 
 
@@ -64,4 +86,54 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    class WeatherTask extends AsyncTask <String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String response = HttpRequest.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=" + CITY + "&units=metric&appid=" + API);
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            if (result != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(result);
+                    JSONObject main = jsonObj.getJSONObject("main");
+                    JSONObject sys = jsonObj.getJSONObject("sys");
+                    JSONObject wind = jsonObj.getJSONObject("wind");
+                    JSONObject weather = jsonObj.getJSONArray("weather").getJSONObject(0);
+
+                    Long updatedAt = jsonObj.getLong("dt");
+                    String updatedAtText = "Последнее обновление: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
+                    String temp = main.getString("temp") + "°C";
+                    String pressure = main.getString("pressure") + " мм. рт. ст.";
+                    String humidity = main.getString("humidity") + "%";
+
+                    Long c = sys.getLong("sunrise");
+                    Long sunset = sys.getLong("sunset");
+                    String windSpeed = wind.getString("speed") + " m/sec";
+                    String weatherDescription = weather.getString("description");
+
+                    String address = jsonObj.getString("name") + ", " + sys.getString("country");
+
+
+                    Log.d(LOG_TAG, address);
+                    Log.d(LOG_TAG, updatedAtText);
+                    Log.d(LOG_TAG, temp);
+                    Log.d(LOG_TAG, pressure);
+                    Log.d(LOG_TAG, humidity);
+                    Log.d(LOG_TAG, sunset.toString());
+
+
+
+                } catch (JSONException e) {
+                    Log.d(LOG_TAG, e.getLocalizedMessage());
+                }
+            }
+        }
+    }
 }
+
